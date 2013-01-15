16205e908cee154a552d98952e5815e105b00565
andreasknoepfle <andreas.knoepfle@googlemail.com>
2013-01-15 20:38:07 +0100

    Futher

--- app.rb
+++ app.rb
@@ -39,12 +39,15 @@ class Event
   key :location,      String
   key :date, Date
   many :items
+  key :members, Array
+  many :mm_users , :in => :members
   timestamps!
 end
 
 class Item
   include MongoMapper::EmbeddedDocument
   belongs_to  :mm_user
+  embedded_in :event
   key :name,    String
   key :amount,   Float
 end
@@ -71,26 +74,65 @@ get '/events/:id' do |id|
    login_required
    @event = Event.find(id)
    @sum = 0
+   @members = MmUser.find @event.members
    @event.items.each do |item|
      @sum += item.amount
    end
    haml :show
 end
 
+get '/events/:id/join' do |id|
+  login_required
+  @event = Event.find(id)
+  @event.push({:members => current_user._id})
+  redirect "/events/#{@event._id}"
+end
+
 post '/events/:id/add' do |id|
    login_required
+  
    @event = Event.find(id)
-   item = Item.new(params[:item])
-   item.mm_user = current_user
-   @event.items << item
-   @event.save
+   if event.members.include? current_user.id
+     item = Item.new(params[:item])
+     item.mm_user = current_user
+     @event.items << item
+     @event.save
+   end
    redirect "/events/#{@event._id}"
 end
 
-get '/events/:id/:item/delete' do |id,item|
+get '/events/:id/:item_id/delete' do |id,item_id|
    login_required
    @event = Event.find(id)
-   @event.pull( :items => {:_id => BSON::ObjectId(item)})
+   
+   @event.items.each do |item|
+     if item.id.to_s == item_id && item.mm_user_id == current_user.id
+        @item = item
+        @event.pull( :items => {:_id => item.id})
+     end
+   end
+
    redirect "/events/#{@event._id}"
-  
+end
+
+get '/events/:id/:item_id/edit' do |id,item_id|
+   login_required
+   @event = Event.find(id)
+   
+   @event.items.each do |item|
+     if item.id.to_s == item_id && item.mm_user_id == current_user.id
+        @item = item
+     end
+   end
+
+   haml :edit
+end
+
+post '/events/:id/:item_id/edit' do |id,item_id|
+   login_required
+   @event = Event.find(id)
+   
+   @event.items.select{|item| item.id.to_s == item_id}.each{}
+
+   haml :edit
 end
